c2bd8e94103ecf03fa296e7235305a224be06574
Yuri Barbosa <127057738+YuriGit909@users.noreply.github.com>
2024-06-08 15:10:59 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,21 @@ btn.addEventListener('click', () => {
         var numero = Number(num.value)
         var res = document.querySelector('select#itabuada')
 
-        // Variável vazia de reset
-        resetHTML = ''
+        // Limpa o select a cada novo clique no botão
+        res.innerHTML = ''
         
-        for(let i = 0; i <= 10; i++){
-            resetHTML += `<option>${numero} x ${i} = ${numero * i}</option>`
-        }
+        for(let i = 1; i <= 10; i++){
+            // Cria um elemento através do JavaScript de forma dinâmica
+            let item = document.createElement('option')
 
-        // A cada novo clique no botão, o valor de "res" será substituído por um espaço vazio
-        res.innerHTML = resetHTML
+            // Conteúdo que irá aparecer dentro do elemento criado acima
+            item.text = `${numero} x ${i} = ${numero * i}`
+
+            // Atributo value para cada option criado no loop, de acordo com a variável i
+            item.value = `item${i}`
+
+            // Acrescenta um novo item (filho) abaixo do criado anteriormente dinamicamente
+            res.appendChild(item)
+        }        
     }
 })
